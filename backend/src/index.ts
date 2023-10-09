18036b079069ae149a100a4d79792b5221d6602a
tiesen <ttien56906@gmail.com>
2023-10-10 06:00:10 +0700

    frist commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,8 @@
-import cors from 'cors'
-import express, { Application } from 'express'
+import app from './app';
 
-import connectToDB from './utils/mongoDb.js'
-import routes from './routes.js'
-
-const PORT: number = parseInt((process.env.PORT as string) || '3000')
-
-const bootstrap = async () => {
-  const app: Application = express()
-  app.use(cors())
-  app.use(express.json())
-
-  await connectToDB()
-
-  routes(app)
-
-  app.listen(PORT, () => {
-    console.log(`
-Server running successfully.
-You can access it here:
-Local:            http://localhost:${PORT}
-On Your Network:  http://192.168.1.9:${PORT}
-Press Ctrl+C to quit.
-`)
-  })
-}
-bootstrap()
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
