c546dfba24bcef2d4d94ce237a58650ba574ced2
Tiesen <ttien56906@gmail.com>
2023-11-01 09:43:40 +0700

    aaa

--- frontend/src/app/template.tsx
+++ frontend/src/app/template.tsx
@@ -1,23 +1,30 @@
 'use client'
 
 import { Container } from '@mui/material'
+import { useEffect, useState } from 'react'
 
 import { Footer, Header, Notification } from '@/components'
 
-const Template: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <main className="flex flex-col gap-3 min-h-screen w-screen select-none">
-    {/* Header */}
-    <Header />
+const Template: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const [mounted, setMounted] = useState<boolean>(false)
+  useEffect(() => setMounted(true), [])
+  if (!mounted) return null
 
-    {/* Content */}
-    <Container className="flex-grow">{children}</Container>
+  return (
+    <main className="flex flex-col gap-3 min-h-screen w-screen select-none">
+      {/* Header */}
+      <Header />
 
-    {/* Actions */}
-    <Notification />
+      {/* Content */}
+      <Container className="flex-grow">{children}</Container>
 
-    {/* Footer */}
-    <Footer />
-  </main>
-)
+      {/* Actions */}
+      <Notification />
+
+      {/* Footer */}
+      <Footer />
+    </main>
+  )
+}
 
 export default Template
