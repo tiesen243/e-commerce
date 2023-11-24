3a5d3e04434c1bc3fa5daa7cbd4e93b78a69d068
Tiesen <ttien56906@gmail.com>
2023-11-24 17:57:00 +0700

    update

--- frontend/src/components/product/create/utils.ts
+++ frontend/src/components/product/create/utils.ts
@@ -2,14 +2,9 @@ import { Category, Tag } from '@/types/product'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
-/* const imageRegex = z */
-/*   .instanceof(File) */
-/*   .refine((val) => val.name, { message: 'Please upload a valid image' }) */
-/*   .refine((val) => val.type.startsWith('image'), { message: 'Please upload an image' }) */
-/*   .refine((val) => val.size < 1024 * 1024 * 5, { message: 'Image must be less than 5MB' }) */
-
 export const createFormSchema = z.object({
   name: z.string().min(4).max(255),
+  image: z.instanceof(File).nullable(),
   description: z.string().min(4).max(255),
   price: z.string().transform((val) => Number(val)),
   stock: z.string().transform((val) => Number(val)),
@@ -22,6 +17,7 @@ export const resolver = zodResolver(createFormSchema)
 export type CreateFormValues = z.infer<typeof createFormSchema>
 export const defaultValues: CreateFormValues = {
   name: '',
+  image: null,
   description: '',
   price: 0,
   stock: 0,
