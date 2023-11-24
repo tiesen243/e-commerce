7e15ad1296d62828a65d34a861f98ab33123353c
Tiesen <ttien56906@gmail.com>
2023-11-24 16:41:33 +0700

    aa

--- frontend/src/app/(manager)/manage/create/page.tsx
+++ frontend/src/app/(manager)/manage/create/page.tsx
@@ -1,11 +1,13 @@
+import CreateForm from '@/components/product/create'
 import { NextPage } from 'next'
-import { Suspense } from 'react'
 
 const Page: NextPage = () => (
   <main>
     <article className="typography">
       <h1>Create new product</h1>
     </article>
+
+    <CreateForm />
   </main>
 )
 
