60b2b50fbc8a6e436e24e33d417beff2f9bf3ea4
Tiesen <ttien56906@gmail.com>
2023-11-26 14:09:21 +0700

    aaaa

--- frontend/src/app/(main)/page.tsx
+++ frontend/src/app/(main)/page.tsx
@@ -1,9 +1,40 @@
+'use client'
+import {
+  Form,
+  FormField,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui'
 import { type NextPage } from 'next'
+import { useForm } from 'react-hook-form'
 
 const Page: NextPage = () => {
+  const form = useForm()
   return (
     <div>
       <h1>Page</h1>
+      <Form {...form}>
+        <form>
+          <FormField
+            name="name"
+            render={() => (
+              <Select>
+                <SelectTrigger>
+                  <SelectValue placeholder="Select a verified email to display" />
+                </SelectTrigger>
+                <SelectContent className="z-50">
+                  <SelectItem value="m@example.com">m@example.com</SelectItem>
+                  <SelectItem value="m@google.com">m@google.com</SelectItem>
+                  <SelectItem value="m@support.com">m@support.com</SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+          />
+        </form>
+      </Form>
     </div>
   )
 }
--- frontend/src/components/product/create/index.tsx
+++ frontend/src/components/product/create/index.tsx
@@ -15,6 +15,7 @@ import DragAndDrop from '@/components/DragAndDrop'
 import Fields, { FieldsProps } from '@/components/Fields'
 import { SelectContent } from '@radix-ui/react-select'
 import { CreateFormValues, defaultValues, resolver } from './config'
+import { Category } from '@/types/product'
 
 const CreateField = Fields as React.FC<FieldsProps<CreateFormValues>>
 
@@ -26,57 +27,56 @@ const CreateForm: React.FC = () => {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-        <CreateField name="name" control={form.control}>
-          {(field) => <Input placeholder={`input your ${field.name}`} {...field} />}
-        </CreateField>
-
-        <CreateField name="image" control={form.control}>
-          {(field) => <DragAndDrop field={field} />}
-        </CreateField>
-
-        <CreateField name="description" control={form.control}>
-          {(field) => <Textarea placeholder={`input your ${field.name}`} {...field} />}
-        </CreateField>
-
-        <CreateField name="category" control={form.control}>
-          {(field) => (
-            <Select>
-              <SelectTrigger>
-                <SelectValue placeholder="Select a verified email to display" />
-              </SelectTrigger>
-              <SelectContent className="z-50">
-                <SelectItem value="m@example.com">m@example.com</SelectItem>
-                <SelectItem value="m@google.com">m@google.com</SelectItem>
-                <SelectItem value="m@support.com">m@support.com</SelectItem>
-              </SelectContent>
-            </Select>
-          )}
-        </CreateField>
-
-        <CreateField name="price" control={form.control}>
-          {(field) => (
-            <Input type="number" placeholder={`input your ${field.name}`} {...field} />
-          )}
-        </CreateField>
-
-        <CreateField name="stock" control={form.control}>
-          {(field) => (
-            <Input type="number" placeholder={`input your ${field.name}`} {...field} />
-          )}
-        </CreateField>
-
-        <Button
-          variant="outline"
-          type="submit"
-          className="w-full"
-          disabled={form.formState.isSubmitting}
-        >
-          {form.formState.isSubmitting && <LoadingSpinner />} Create
-        </Button>
-      </form>
-    </Form>
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="z-10 space-y-4">
+          <CreateField name="name" control={form.control}>
+            {(field) => <Input placeholder={`input your ${field.name}`} {...field} />}
+          </CreateField>
+          Other
+          <CreateField name="image" control={form.control}>
+            {(field) => <DragAndDrop field={field} />}
+          </CreateField>
+          <CreateField name="description" control={form.control}>
+            {(field) => <Textarea placeholder={`input your ${field.name}`} {...field} />}
+          </CreateField>
+          <CreateField name="category" control={form.control}>
+            {(field) => (
+              <Select onValueChange={() => field.onChange} defaultValue={field.value}>
+                <SelectTrigger>
+                  <SelectValue placeholder="Select a product category" />
+                </SelectTrigger>
+                <SelectContent className="z-50">
+                  {Object.keys(Category).map((category) => (
+                    <SelectItem key={category} value={category}>
+                      {category}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            )}
+          </CreateField>
+          <CreateField name="price" control={form.control}>
+            {(field) => (
+              <Input type="number" placeholder={`input your ${field.name}`} {...field} />
+            )}
+          </CreateField>
+          <CreateField name="stock" control={form.control}>
+            {(field) => (
+              <Input type="number" placeholder={`input your ${field.name}`} {...field} />
+            )}
+          </CreateField>
+          <Button
+            variant="outline"
+            type="submit"
+            className="w-full"
+            disabled={form.formState.isSubmitting}
+          >
+            {form.formState.isSubmitting && <LoadingSpinner />} Create
+          </Button>
+        </form>
+      </Form>
+    </>
   )
 }
 
