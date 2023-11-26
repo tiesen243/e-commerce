f3df8164f389f7e85aa74c11ec8947483cfd8991
Tiesen <ttien56906@gmail.com>
2023-11-26 13:24:37 +0700

    aaa

--- frontend/src/components/product/create/index.tsx
+++ frontend/src/components/product/create/index.tsx
@@ -1,9 +1,20 @@
-import { Button, Form, Input, LoadingSpinner, Textarea } from '@/components/ui'
+import {
+  Button,
+  Form,
+  Input,
+  LoadingSpinner,
+  Select,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+  Textarea,
+} from '@/components/ui'
 import { useForm } from 'react-hook-form'
 
 import DragAndDrop from '@/components/DragAndDrop'
-import { CreateFormValues, defaultValues, resolver } from './config'
 import Fields, { FieldsProps } from '@/components/Fields'
+import { SelectContent } from '@radix-ui/react-select'
+import { CreateFormValues, defaultValues, resolver } from './config'
 
 const CreateField = Fields as React.FC<FieldsProps<CreateFormValues>>
 
@@ -27,6 +38,21 @@ const CreateForm: React.FC = () => {
 
         <CreateField name="description" control={form.control}>
           {(field) => <Textarea placeholder={`input your ${field.name}`} {...field} />}
+        </CreateField>
+
+        <CreateField name="category" control={form.control}>
+          {(field) => (
+            <Select>
+              <SelectTrigger>
+                <SelectValue placeholder="Select a verified email to display" />
+              </SelectTrigger>
+              <SelectContent className="z-50">
+                <SelectItem value="m@example.com">m@example.com</SelectItem>
+                <SelectItem value="m@google.com">m@google.com</SelectItem>
+                <SelectItem value="m@support.com">m@support.com</SelectItem>
+              </SelectContent>
+            </Select>
+          )}
         </CreateField>
 
         <CreateField name="price" control={form.control}>
