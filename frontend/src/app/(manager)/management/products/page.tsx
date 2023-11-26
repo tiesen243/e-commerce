0d27f90e97692d5fa3d2744d913dca5a2ca3fc47
Tiesen <ttien56906@gmail.com>
2023-11-26 16:53:06 +0700

    aaa

--- frontend/src/app/(manager)/management/products/page.tsx
+++ frontend/src/app/(manager)/management/products/page.tsx
@@ -1,14 +1,6 @@
-'use client'
-import axios from 'axios'
 import { NextPage } from 'next'
 
 const Page: NextPage = () => {
-  fetch('/api/product/me', {})
-    .then((res) => res.json())
-    .then((res) => {
-      console.log(res)
-    })
-
   return <div>Page</div>
 }
 
