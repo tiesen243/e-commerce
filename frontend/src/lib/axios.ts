441da2298e73544ac8cf3024fb07dca73c77689a
Tiesen <ttien56906@gmail.com>
2023-12-06 09:04:20 +0700

    aaa

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://yuki-api.vercel.app',
+  baseURL: 'https://tiesen-api.vercel.app',
   headers: {
     'Content-type': 'application/json',
   },
