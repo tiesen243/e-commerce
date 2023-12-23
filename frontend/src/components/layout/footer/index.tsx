ceeb91dcbafb39609ef6f22235ff35af99fefa1e
Tiesen <ttien56906@gmail.com>
2023-12-23 16:05:44 +0700

    aaaa

--- frontend/src/components/layout/footer/index.tsx
+++ frontend/src/components/layout/footer/index.tsx
@@ -8,7 +8,7 @@ const copyrightName = 'TN'
 const copyrightDate = new Date().getFullYear()
 const Footer: React.FC = () => (
   <footer className="rounded-lg border bg-card text-card-foreground shadow-sm">
-    <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0 dark:border-neutral-700">
+    <div className="container mx-auto flex w-full max-w-7xl flex-col gap-6 px-6 py-12 text-sm md:flex-row md:gap-12 md:px-0">
       <div>
         <Link className="flex items-center gap-2 text-black md:pt-1 dark:text-white" href="/">
           <div className="flex size-7 flex-none items-center justify-center rounded-lg border bg-background">
@@ -31,7 +31,7 @@ const Footer: React.FC = () => (
         <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
         <p>Designed in Vietnam</p>
         <p className="md:ml-auto">
-          <a href="https://vercel.com" className="text-black dark:text-white">
+          <a href="https://tiesen.id.vn" className="text-black dark:text-white">
             Crafted by Tiesen
           </a>
         </p>
