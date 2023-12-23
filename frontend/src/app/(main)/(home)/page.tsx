ad3dfebc9c244599bd754f99a2b2128ff914f652
Tiesen <ttien56906@gmail.com>
2023-12-23 16:07:40 +0700

    revalidate data

--- frontend/src/app/(main)/(home)/page.tsx
+++ frontend/src/app/(main)/(home)/page.tsx
@@ -3,6 +3,7 @@ import { type NextPage } from 'next'
 import ThreeItemsGrid from '@/components/home/threeItemsGrid'
 import Carousel from '@/components/home/carousel'
 
+export const revalidate = 10
 const Page: NextPage = async () => (
   <>
     <ThreeItemsGrid />
